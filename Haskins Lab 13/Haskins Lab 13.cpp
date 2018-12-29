221580bd582d8b9366caef11a294978bf950ac38
mark.haskins11792 <mark.haskins11792@gmail.com>
2018-12-29 16:47:18 -0600

    importing from VS

--- Haskins Lab 13/Haskins Lab 13.cpp
+++ Haskins Lab 13/Haskins Lab 13.cpp
@@ -1,10 +1,4 @@
-//***********************************************************************
-// Name: Mark Haskins
-// Date: 11/23/2018
-// Lab: 13
-// Description: Prints a random list of the integers 0-9, sorts and reprints the list, and removes even numbers and reprints the list. 	
-//***********************************************************************
-
+// Prints a random list of the integers 0-9, sorts and reprints the list, and removes even numbers and reprints the list. 	
 
 #include "stdafx.h"
 #include <iostream>
@@ -12,7 +6,6 @@
 #include <algorithm>
 using namespace std;
 
-//use a predicate to remove the even numbers, then print the list.
 bool even(int a)
 {
 	return (a % 2) == 0;
@@ -20,15 +13,15 @@ bool even(int a)
 
 int main()
 {
-	vector <int> data{ 0,1,2,3,4,5,6,7,8,9 }; //Create an STL vector of 10 integers and store initial values 0 to 9 (data[0] == 0, data[1] == 1, etc.).
-	random_shuffle(begin(data), end(data)); // Use the std::random_shuffle algorithm to shuffle the list.
+	vector <int> data{ 0,1,2,3,4,5,6,7,8,9 };
+	random_shuffle(begin(data), end(data));
 	cout << "Random Shuffle:\n";
-	for (auto it = begin(data); it != end(data); it++) //Print the list. (use an iterator in a regular for loop)
+	for (auto it = begin(data); it != end(data); it++)
 		cout << *it << ' ';
 	cout << endl << endl;
-	sort(begin(data), end(data)); //Use the std::sort algorithm to sort the list.
+	sort(begin(data), end(data)); 
 	cout << "Sort:\n";
-	for (auto i : data) //Print the list again. (use a range - based for loop)
+	for (auto i : data) 
 		cout << i << ' ';
 	cout << endl << endl;
 	auto newEnd = remove_if(begin(data), end(data), even);
